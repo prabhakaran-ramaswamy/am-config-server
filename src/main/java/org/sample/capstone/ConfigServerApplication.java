e6f351887d94c74e858d432e0310883729555a02
pramaswamy <pkaran.ramaswamy@gmail.com>
2020-01-24 10:22:51 +0530

    changes

--- src/main/java/org/sample/capstone/ConfigServerApplication.java
+++ src/main/java/org/sample/capstone/ConfigServerApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 @EnableConfigServer
 @SpringBootApplication
-public class AMConfigServerApplication {
+public class ConfigServerApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(AMConfigServerApplication.class, args);
+		SpringApplication.run(ConfigServerApplication.class, args);
 	}
 }
